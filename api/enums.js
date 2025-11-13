df609b28082c9ea50dc96d666e364b6ae9846303
Deislher Yadir Sánchez Funez <125695457+DeislherSanchez@users.noreply.github.com>
2025-11-12 18:45:12 -0600

    ajustes en el endpoint

--- api/enums.js
+++ api/enums.js
@@ -1,26 +1,25 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/enums', (req, res) => {
-  res.json({
-    CategoriaPerfume: [
-      "ExtraitDeParfum",
-      "Parfum",
-      "EauDeParfum",
-      "EauDeToilette",
-      "EauFraiche",
-      "Elixir"
-    ],
-    Genero: [
-      "Male",
-      "Female",
-      "Unisex"
-    ],
-    Role: [
-      "USER",
-      "ADMIN"
-    ]
-  });
-});
-
-export default router;
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({
+      CategoriaPerfume: [
+        "ExtraitDeParfum",
+        "Parfum",
+        "EauDeParfum",
+        "EauDeToilette",
+        "EauFraiche",
+        "Elixir"
+      ],
+      Genero: [
+        "Male",
+        "Female",
+        "Unisex"
+      ],
+      Role: [
+        "USER",
+        "ADMIN"
+      ]
+    });
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
